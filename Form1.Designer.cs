bca9cdfe31650d99be7a1ea3b6e4c98165826452
leonardo.ocruz <leonardo.ocruz@PIR06305W10-1>
2022-05-25 14:32:37 -0300

    entrada_menor_de_idade

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace entrada_menor_de_idade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_result = new System.Windows.Forms.Label();
+            this.txt_idade = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(493, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DIgite a Idade:";
+            // 
+            // lb_result
+            // 
+            this.lb_result.AutoSize = true;
+            this.lb_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_result.Location = new System.Drawing.Point(44, 196);
+            this.lb_result.Name = "lb_result";
+            this.lb_result.Size = new System.Drawing.Size(64, 13);
+            this.lb_result.TabIndex = 2;
+            this.lb_result.Text = "Resultado";
+            // 
+            // txt_idade
+            // 
+            this.txt_idade.Location = new System.Drawing.Point(120, 131);
+            this.txt_idade.Name = "txt_idade";
+            this.txt_idade.Size = new System.Drawing.Size(100, 20);
+            this.txt_idade.TabIndex = 3;
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button.Location = new System.Drawing.Point(206, 191);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 5;
+            this.button.Text = "VERIFICAR";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 276);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.txt_idade);
+            this.Controls.Add(this.lb_result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_result;
+        private System.Windows.Forms.TextBox txt_idade;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace entrada_menor_de_idade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (txt_idade.Text != "")
+            {
+
+
+
+                int idade = Convert.ToInt32(txt_idade.Text);
+
+                if (idade >= 18)
+                {
+
+                    lb_result.Text = "PERMISSÃO CONCEDIDA";
+                    lb_result.ForeColor = Color.Green;
+                    lb_result.Font = new Font("Arial", 13, FontStyle.Bold);
+
+
+                }
+                else
+                {
+
+                    lb_result.Text = "SEM PERMISSÃO";
+                    lb_result.ForeColor = Color.Red;
+                    lb_result.Font = new Font("Arial", 15, FontStyle.Bold);
+
+
+
+                }
+
+
+
+
+
+
+            }
+            else
+            {
+
+                MessageBox.Show("Favor preencher o campo corretamente", "ATENÇÃO!!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+
+            }
+        }
+    }
+}
